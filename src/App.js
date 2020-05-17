60e7d76f19a6a42ac8e5f73a610a0da0b99abd73
Manoj Kumar S <manojkumars@sahajsoft.com>
2020-05-17 13:23:11 +0530

    Sets up material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
@@ -7,6 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2>Tasks Management</h2>
+          <Button variant="contained" color="primary">
+            Add tasks
+          </Button>
         </div>
       </div>
     );
