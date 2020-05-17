706c7775268c9cd62b2b2c19152d9a2e9b19de56
Manoj Kumar S <manojkumars@sahajsoft.com>
2020-05-17 14:18:59 +0530

    set up material ui global color theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#5995de',
+      main: '#1568ac',
+      dark: '#003e7c',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffffff',
+      main: '#f5f5f5',
+      dark: '#c2c2c2',
+      contrastText: '#000',
+    },
+  },
+  status: {
+    danger: 'red',
+  },
+});
+theme = responsiveFontSizes(theme);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Tasks Management</h2>
-          <Button variant="contained" color="primary">
-            Add tasks
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <div className="App-header">
+            <h2>Tasks Management</h2>
+            <Button variant="contained" color="primary">
+              Add tasks
           </Button>
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     );
   }
 }
